1f5eed9a607a05d9aa144cc9128e0f8fb16aac62
조성호 <eoblue23@naver.com>
2024-11-08 13:59:07 +0900

    ✨ 데이터 추가 신청, 신청 목록 확인, 신청 승인 기능 개발
    .

--- ezdg-auto/src/main/java/com/openmind/ezdg/user/repository/DataApplyRepository.java
+++ ezdg-auto/src/main/java/com/openmind/ezdg/user/repository/DataApplyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DataApplyRepository extends JpaRepository<DataApplyEntity, Integer> {
+public interface DataApplyRepository extends JpaRepository<DataApplyEntity, Long> {
 }
--- ezdg-auto/src/main/java/com/openmind/ezdg/user/service/UserService.java
+++ ezdg-auto/src/main/java/com/openmind/ezdg/user/service/UserService.java
@@ -6,16 +6,34 @@ import com.openmind.ezdg.user.repository.DataApplyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private DataApplyRepository dataApplyRepository;
+    final private DataApplyRepository dataApplyRepository;
 
     public void applyData(DataApplyDto dataApplyDto) {
         DataApplyEntity dataApplyEntity = DataApplyEntity.toSaveEntity(dataApplyDto);
 
         dataApplyRepository.save(dataApplyEntity);
 
+    }
+
+    public List<DataApplyEntity> getAllDataApply() {
+        return dataApplyRepository.findAll();
+    }
+
+    public boolean acceptDataApply(Long id) {
+        Optional<DataApplyEntity> dataApplyEntityOpt = dataApplyRepository.findById(id);
+        if (dataApplyEntityOpt.isPresent()) {
+            DataApplyEntity dataApplyEntity = dataApplyEntityOpt.get();
+            dataApplyEntity.setIsAccepted(true); // isAccepted 값을 true로 설정
+            dataApplyRepository.save(dataApplyEntity); // 업데이트된 엔티티 저장
+            return true;
+        }
+        return false;
     }
 }
