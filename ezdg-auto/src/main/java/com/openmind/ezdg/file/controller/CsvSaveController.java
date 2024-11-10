9bda72d4e504c44c843f26909ce2165cafcf2a31
hajae <gkrhf@naver.com>
2024-11-10 16:41:25 +0900

    :recycle: refactoring admin-file

--- ezdg-auto/src/main/java/com/openmind/ezdg/file/controller/CsvSaveController.java
+++ ezdg-auto/src/main/java/com/openmind/ezdg/file/controller/CsvSaveController.java
@@ -1,5 +1,6 @@
 package com.openmind.ezdg.file.controller;
 
+import com.openmind.ezdg.file.dto.filesave.FileDataRequestDto;
 import com.openmind.ezdg.file.dto.filesave.FileInfoDto;
 import com.openmind.ezdg.file.dto.filesave.ValidateDuplicateCodeDto;
 import com.openmind.ezdg.file.service.CsvSaveService;
@@ -8,15 +9,20 @@ import com.openmind.ezdg.file.util.CsvUtil;
 import com.openmind.ezdg.file.util.FileUtil;
 import com.openmind.ezdg.generate.library.file.service.JavaFileLibraryGenerateService;
 import com.openmind.ezdg.generate.server.service.APIServerGenerateService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -32,67 +38,84 @@ public class CsvSaveController {
     private final CsvUtil csvUtil;
     private final FileUtil fileUtil;
 
+    @Value("${admin.base-url}")
+    private String baseUrl;
+
     /**
      * 파일 변환
      */
     @PostMapping()
-    public String translateFile(@RequestParam("file") MultipartFile file, @RequestParam("code") String code,
-                                RedirectAttributes redirectAttributes, Model model) {
+    @ResponseBody
+    public Map<String, Object> translateFile(@RequestParam("file") MultipartFile file,
+                                             @RequestParam("code") String code) {
         log.info("[FileSaveController] request save file for {}", file.getOriginalFilename());
-
-        // 중복 코드 검증
-        ValidateDuplicateCodeDto validateDuplicateCode = csvSaveService.validateDuplicateCode(code);
-        if (validateDuplicateCode.getIsDuplicate()) {
-            model.addAttribute("isDuplicateCode", true);
-            model.addAttribute("regDate", validateDuplicateCode.getRegDate());
-            return "views/filesave/duplicate";
-        }
+        Map<String, Object> response = new HashMap<>();
 
         boolean isSuccessFileSaveToTempPath = fileUtil.saveFileToTempPath(file);
         if(!isSuccessFileSaveToTempPath) {
-            return "views/filesave/error";
+            response.put("isSuccess", false);
+            response.put("error", "파일을 저장하는 중 오류가 발생했습니다.");
+            return response;
         }
 
         // 파일 이름 한<->영 번역 및 컨벤션 적용
         String translatedFileName = csvSaveService.getTranslatedFileName(file.getOriginalFilename());
         log.info("translated file name = {}", translatedFileName);
 
-        // view 전달 파라미터 설정
-        redirectAttributes.addFlashAttribute("originFileName", file.getOriginalFilename());
-        redirectAttributes.addFlashAttribute("translatedFileName", translatedFileName);
-
-        // read csv
+        // csv 파일 읽기 및 칼럼 변환
         String filePath = fileUtil.getFullPath(file.getOriginalFilename());
         List<String[]> datas = csvUtil.readCsvFile(filePath);
-
-        // 칼럼 이름 한<->영 번역 및 컨벤션 적용
         List<String> originalColumns = csvSaveService.getOriginalColumns(datas);
         List<String> translatedColumns = csvSaveService.getTranslatedColumns(originalColumns);
 
         log.info("original columns = {}", originalColumns);
         log.info("translated columns = {}", translatedColumns);
 
-        // view 전달 파라미터 설정
-        redirectAttributes.addFlashAttribute("originalColumns", originalColumns);
-        redirectAttributes.addFlashAttribute("translatedColumns", translatedColumns);
+        // 성공 응답 구성
+        response.put("isSuccess", true);
+        if("/admin".equals(baseUrl)) {
+            response.put("redirectUrl", "/file/save");
+        } else if("".equals(baseUrl)) {
+            response.put("redirectUrl", "/admin/file/save");
+        }
 
-        redirectAttributes.addFlashAttribute("fileName", file.getOriginalFilename());
-        redirectAttributes.addFlashAttribute("code", code);
+        response.put("originFileName", file.getOriginalFilename());
+        response.put("translatedFileName", translatedFileName);
+        response.put("originalColumns", originalColumns);
+        response.put("translatedColumns", translatedColumns);
+        response.put("fileName", file.getOriginalFilename());
+        response.put("code", code);
 
-        return "redirect:/file/save";
+        return response;
+    }
+
+    /**
+     * 파일 저장 페이지 호출
+     */
+    @PostMapping("/save-data")
+    @ResponseBody
+    public ResponseEntity<Void> saveFileData(@RequestBody Map<String, Object> data, HttpSession session) {
+        log.info("[FileSaveController] request file save data {}", data.toString());
+        session.setAttribute("fileData", data);
+        return ResponseEntity.ok().build();
     }
 
     /**
      * 파일 저장 페이지 호출
      */
     @GetMapping("/save")
-    public String getFileSavePage(Model model) {
-        log.info("파일 저장 페이지 호출");
-        model.addAttribute("fileName", model.getAttribute("fileName"));
-        model.addAttribute("code", model.getAttribute("code"));
-        model.addAttribute("originalFileName", model.getAttribute("originFileName"));
-        model.addAttribute("originalColumns", model.getAttribute("originalColumns"));
-
+    public String getFileSavePage(HttpSession session, Model model) {
+        log.info("[FileSaveController] request get file save page");
+        Map<String, Object> fileData = (Map<String, Object>) session.getAttribute("fileData");
+        if(fileData != null) {
+            model.addAttribute("fileData", fileData);
+            session.removeAttribute("fileData");
+        }
+        if("/admin".equals(baseUrl)) {
+            model.addAttribute("baseUrl", "");
+        } else if("".equals(baseUrl)) {
+            model.addAttribute("baseUrl", "/admin");
+        }
         return "views/file/translate";
     }
 
@@ -100,59 +123,38 @@ public class CsvSaveController {
      * 파일 저장
      */
     @PostMapping("/save")
-    public String saveFile(@RequestParam(value = "translatedFileName") String translatedFileName,
-                           @RequestParam(value = "translatedColumns") List<String> translatedColumns,
-                           @RequestParam(value = "fileName") String fileName,
-                           @RequestParam(value = "code") String code,
-                           @RequestParam(value = "originalFileName") List<String> originalFileName,
-                           @RequestParam(value = "originalColumns") List<String> originalColumns,
-                           RedirectAttributes redirectAttributes) {
+    public String saveFile(@RequestBody FileDataRequestDto fileData) {
+        String translatedFileName = fileData.getTranslatedFileName();
+        List<String> translatedColumns = fileData.getTranslatedColumns();
+        String originFileName = fileData.getOriginFileName();
+        List<String> originalColumns = fileData.getOriginColumns();
+        String code = fileData.getCode();
+
         log.info("save file request for {}", translatedFileName);
         log.info("translatedColumns = {}", translatedColumns);
 
-        MultipartFile file = fileUtil.readFileFromTempPath(fileName);
+        MultipartFile file = fileUtil.readFileFromTempPath(originFileName);
 
         // read csv
         String filePath = fileUtil.getFullPath(file.getOriginalFilename());
         List<String[]> datas = csvUtil.readCsvFile(filePath);
 
-        // 파일 삭제
-        fileUtil.deleteFileFromTempPath(fileName);
-
         // mongoDB에 저장
         csvSaveService.saveFile(datas, translatedFileName, translatedColumns);
 
-        // public data code 저장
-        csvSaveService.insertCode(code);
+        // TODO mongoDB에 저장정보 저장
 
         // library 자동화를 위해 DTO 생성
-        FileInfoDto fileInfoDto = sendAutoLibraryInfoService.setBasicInfo(fileName, translatedFileName, originalColumns);
+        FileInfoDto fileInfoDto = sendAutoLibraryInfoService.setBasicInfo(originFileName, translatedFileName, originalColumns);
         sendAutoLibraryInfoService.makeAutoLibraryInfo(fileInfoDto, translatedColumns, datas);
 
         apiServerGenerateService.generate(fileInfoDto);
         javaFileLibraryGenerateService.generate(fileInfoDto);
 
-        // view 전달 파라미터
-//        redirectAttributes.addFlashAttribute("collection", translatedFileName);
-//        redirectAttributes.addFlashAttribute("code", code);
+        // 파일 삭제
+        fileUtil.deleteFileFromTempPath(originFileName);
 
         return "views/file/file-complete";
     }
 
-    /**
-     * 저장 결과
-     */
-    /*
-    @GetMapping("/result")
-    public String getResultPage(Model model) {
-        String collectionName = (String) model.getAttribute("collection");
-
-        // db에 잘 들어갔는지, db에서 조회해서 결과 출력
-        List<List<MongoBsonValueDto>> result = csvSaveService.getSavedData(collectionName);
-
-        model.addAttribute("result", result);
-
-        return "views/filesave/result";
-    }
-     */
 }
