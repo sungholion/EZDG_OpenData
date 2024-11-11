ecf29cd7791802aec4dd03f0b4b9a440a7bf013b
박형준 <issg2030@gmail.com>
2024-11-11 10:04:04 +0900

    ✨ OpenAPI 데이터 저장서비스

--- ezdg-auto/src/main/java/com/openmind/ezdg/datalist/dto/ApiDataDto.java
+++ ezdg-auto/src/main/java/com/openmind/ezdg/datalist/dto/ApiDataDto.java
@@ -1,4 +1,15 @@
 package com.openmind.ezdg.datalist.dto;
 
+import com.openmind.ezdg.generate.library.openapi.FastApiResponseDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class ApiDataDto {
+    public String mainTitle;
+    public String mainDescription;
+    public List<FastApiResponseDto> apiList;
 }
