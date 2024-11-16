e63466970cb1ddcade2c461908da53a967fbf716
hajae <gkrhf@naver.com>
2024-11-16 14:53:55 +0900

    :recycle: dockerfile 작성 및 baseurl 수정

--- ezdg-sample/daegu-travel-frontend/src/api/travelApi.js
+++ ezdg-sample/daegu-travel-frontend/src/api/travelApi.js
@@ -1,22 +1,22 @@
 // src/api/travelApi.js
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
- baseURL: 'https://k11d201.p.ssafy.io',
- timeout: 5000,
+  baseURL: "43.202.254.160:8080",
+  timeout: 5000,
 });
 
 export const fetchTourData = async () => {
- const response = await api.get('/daeguTravelDB/getAllDaeguTourism');
- return response.data;
+  const response = await api.get("/daeguTravelDB/getAllDaeguTourism");
+  return response.data;
 };
 
 export const fetchHotelData = async () => {
- const response = await api.get('/daeguTravelDB/getAllDaeguAccommodation');
- return response.data;
+  const response = await api.get("/daeguTravelDB/getAllDaeguAccommodation");
+  return response.data;
 };
 
 export const fetchFoodData = async () => {
- const response = await api.get('/daeguTravelDB/getAllDaeguRestaurant');
- return response.data;
+  const response = await api.get("/daeguTravelDB/getAllDaeguRestaurant");
+  return response.data;
 };
