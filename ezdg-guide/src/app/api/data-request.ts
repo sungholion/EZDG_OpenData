872b2ebd6f5e4e422f44d63fb859dda839ee60db
박민규 <tig04162@gmail.com>
2024-11-18 22:45:29 +0900

    🐛 데이터 요청 response 버그 수정
    .

--- ezdg-guide/src/app/api/data-request.ts
+++ ezdg-guide/src/app/api/data-request.ts
@@ -3,7 +3,7 @@ import { FormValues } from "@/types/form";
 
 export const dataRequestAPI = {
   requestData: async (formData: FormValues) => {
-    const response = await api.post("/admin/users/dataApply", formData)
+    const response = await api.post("/users/dataApply", formData)
     return response.data;
   }
 }
