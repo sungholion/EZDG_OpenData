f205ecc06490ea09ff96cc41424b26e005e1657c
박민규 <tig04162@gmail.com>
2024-11-18 21:06:53 +0900

    💄 설치 가이드 코드 변경
    .

--- ezdg-guide/src/components/installation/java.tsx
+++ ezdg-guide/src/components/installation/java.tsx
@@ -11,19 +11,17 @@ const JAVA_EXAMPLES: CodeExample[] = [
 }
 
 dependencies {
-	implementation 'com.openmind:ezdg:v1.0.0'
+	implementation 'com.openmind:ezdg:v1.10.0'
 }`,
     language: "java",
   },
   {
     title: "사용 예제",
-    description: "EZDG를 이용한 기상청 초단기실황조회",
+    description: "EZDG를 이용한 기상청 초단기실황조회(OpenAPI 데이터 형태)",
     code: `public static void main(String[] args) {
-        VilageFcstApi api = new VilageFcstApi ()
-                .ServiceKey("YOUR_SERVICE_KEY") // 발급받은 서비스 키를 여기에 입력") // 발급받은 서비스 키를 여기에 입력
-                .pageNo("1")
-                .numOfRows("10")
-                .dataType("JSON")
+        VilageFcstApi api = new VilageFcstApi("YOUR_SERVICE_KEY")
+                .pageNo(1)
+                .numOfRows(10)
                 .base_date("20241112") // 예시 날짜
                 .base_time("0630") // 예시 시간
                 .nx("60")
@@ -36,20 +34,14 @@ dependencies {
     language: "java",
   },
   {
-    description: "EZDG를 이용한 기상청 초단기예보조회",
+    description: "EZDG를 이용한 대구 음식점 등록현황(파일 데이터 형태)",
     code: `public static void main(String[] args) {
-        UltraSrtFcstApi api = new UltraSrtFcstApi()
-                .ServiceKey("YOUR_SERVICE_KEY") // 발급받은 서비스 키를 여기에 입력") // 발급받은 서비스 키를 여기에 입력
-                .pageNo("1")
-                .numOfRows("10")
-                .dataType("JSON")
-                .base_date("20241112") // 예시 날짜
-                .base_time("0630") // 예시 시간
-                .nx("60")
-                .ny("127");
+        DaeguRestaurantAPI api = new DaeguRestaurantAPI()
+                .pageNo(1)
+                .numOfRows(100);
 
         // API 호출 및 응답 출력
-        UltraSrtFcstApiResponse response = api.fetch();
+        List<DaeguRestaurant> response = api.fetch();
         System.out.println(response);
     }`,
     language: "java",
