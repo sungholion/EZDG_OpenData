0cdeac52d65a4d000dc6339060dd5492a2154b28
박형준 <issg2030@gmail.com>
2024-11-18 14:36:55 +0900

    :bug: fix typo

--- ezdg-javalib/src/main/java/com/openmind/ezdg/common/Encoder.java
+++ ezdg-javalib/src/main/java/com/openmind/ezdg/common/Encoder.java
@@ -4,7 +4,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class Encoder {
-    public String encode(String value) {
+    public static String encode(String value) {
         try {
             return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
         } catch (Exception e) {
@@ -12,7 +12,7 @@ public class Encoder {
         }
     }
 
-    public String encode(Number value){
+    public static String encode(Number value){
         return encode(value.toString());
     }
 }
