107173ffbce4e36f7565f9dddae9fb206b26c46e
gkrhf@naver.com <gkrhf@naver.com>
2024-11-18 08:16:37 +0000

    :rocket: 2024-11-18 08:16:37 deploy

---
+++ ezdg-javalib/src/main/java/com/openmind/ezdg/daegu_accommodation/DaeguAccommodation.java
@@ -0,0 +1,116 @@
+package com.openmind.ezdg.daegu_accommodation;
+
+
+public class DaeguAccommodation {
+
+    private Long number;
+    private String region;
+    private String industryClassification;
+    private String hotelName;
+    private Long guestMistakeStard;
+    private Long guestMistakeDeluxe;
+    private Long guestMistakeSuite;
+    private Long guestMistakeOneRoom;
+    private String guestMistakeOther;
+    private Long guestMistakeCount;
+    private String location;
+    private String phoneNumber;
+
+    // Getters and Setters
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getIndustryClassification() {
+        return industryClassification;
+    }
+
+    public void setIndustryClassification(String industryClassification) {
+        this.industryClassification = industryClassification;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public Long getGuestMistakeStard() {
+        return guestMistakeStard;
+    }
+
+    public void setGuestMistakeStard(Long guestMistakeStard) {
+        this.guestMistakeStard = guestMistakeStard;
+    }
+
+    public Long getGuestMistakeDeluxe() {
+        return guestMistakeDeluxe;
+    }
+
+    public void setGuestMistakeDeluxe(Long guestMistakeDeluxe) {
+        this.guestMistakeDeluxe = guestMistakeDeluxe;
+    }
+
+    public Long getGuestMistakeSuite() {
+        return guestMistakeSuite;
+    }
+
+    public void setGuestMistakeSuite(Long guestMistakeSuite) {
+        this.guestMistakeSuite = guestMistakeSuite;
+    }
+
+    public Long getGuestMistakeOneRoom() {
+        return guestMistakeOneRoom;
+    }
+
+    public void setGuestMistakeOneRoom(Long guestMistakeOneRoom) {
+        this.guestMistakeOneRoom = guestMistakeOneRoom;
+    }
+
+    public String getGuestMistakeOther() {
+        return guestMistakeOther;
+    }
+
+    public void setGuestMistakeOther(String guestMistakeOther) {
+        this.guestMistakeOther = guestMistakeOther;
+    }
+
+    public Long getGuestMistakeCount() {
+        return guestMistakeCount;
+    }
+
+    public void setGuestMistakeCount(Long guestMistakeCount) {
+        this.guestMistakeCount = guestMistakeCount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
